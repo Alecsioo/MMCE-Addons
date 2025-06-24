ccf9cefab64642e323aa6735bf5d4c8b8052f99b
Alecsioo <141121073+Alecsioo@users.noreply.github.com>
2025-06-24 10:16:43 +0200

    Scrubber hatches rework, added inactive radiation sponge and radiation sponge, added debug command to dump the scrubber cache

--- src/main/java/github/alecsio/mmceaddons/common/config/MMCEAConfig.java
+++ src/main/java/github/alecsio/mmceaddons/common/config/MMCEAConfig.java
@@ -6,6 +6,9 @@ import net.minecraftforge.common.config.Config;
 @Config(modid = ModularMachineryAddons.MODID)
 public class MMCEAConfig {
 
+    @Config.Comment({"The amount of radiation that. Default: 10000"})
+    public static int spongeMaxRadiation = 10000;
+
     @Config.Comment("The cooldown between multiblock assemblies. Default: 1 second")
     public static int cooldown = 1;
 
